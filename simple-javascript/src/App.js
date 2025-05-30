988c31e69876ae1c0aa63ee5736c9c095fd456d9
Wadeyyy415 <96021965+FRANZ2103@users.noreply.github.com>
2025-05-31 05:47:05 +0800

    Understanding Mapping

--- simple-javascript/src/App.js
+++ simple-javascript/src/App.js
@@ -1,9 +1,36 @@
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.randomuser.me/?nat=US&results=5")
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }, []);
+
   return (
-    <div className="App"> 
-    We will be right back!
+    <div className="App">
+      {
+      this.state.users.map( user => <div>{user.cell}</div>)
+      
+      
+      
+      /* <h1>User List</h1>
+      {users.length === 0 ? (
+        <p>Loading users...</p>
+      ) : (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )} */}
     </div>
   );
 }

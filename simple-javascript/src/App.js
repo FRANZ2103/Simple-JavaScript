9d033263a911dbe5fe577a4c93e46bd747f4e4d8
Wadeyyy415 <96021965+FRANZ2103@users.noreply.github.com>
2025-06-23 02:42:54 +0800

    Before wiping

--- simple-javascript/src/App.js
+++ simple-javascript/src/App.js
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+const dataArray = [
+  {name: 'First Data', id: 1},
+  {name: 'Second Data', id:2},
+  {name: 'Third Data', id: 3},
+];
+
+const anotherListItems = dataArray.map(dataArray =>
+  <li key={dataArray.id}>
+    {dataArray.name}
+  </li>
+)
 
 //declare a user function
+
 
 const profile1 ={
   name: 'Franz Sundiam',
@@ -10,8 +22,40 @@ const profile1 ={
   imageSize:90
 };
 
+const products= [
+  {title: 'Cabbage', id:'1'},
+  {title: 'Apple', id: '2'}
+]
+
+const listItems = products.map(products =>
+          <li key={products.id}>
+            {products.title}
+          </li>
+          )
+
+function MyButton(){
+  // This is used as updater
+  // Set something, update
+  const [count, setCount] = useState(0);
+  function handleClick(){
+    setCount(count + 1 )
+  };
+ 
+  const button_amount = 2;
+
+  return (
+  <button onClick={handleClick}>
+    I'm a Button! {count}
+  </button>
+  );
+}
+
+
+
 function App() {
   const [users, setUsers] = useState([]);
+
+
 
   useEffect(() => {
     axios.get("https://api.randomuser.me/?nat=US&results=5")
@@ -24,13 +68,7 @@ function App() {
   }, []);
 
 
-function MyButton(){
-  return (
-  <button>
-    I'm a Button!
-  </button>
-  );
-}
+
   return (
     <div className="App">
       {
@@ -47,6 +85,9 @@ function MyButton(){
             height: profile1.imageSize,
             width: profile1.imageSize
             }} ></img>
+          
+          <ul>{anotherListItems}</ul>
+          
           
           </div>
           </>
---
+++ simple-javascript/src/Nnew.js
@@ -0,0 +1,6 @@
+// nnew.js
+import React from "react";
+
+export default function Nnew() {
+  return <h1>This is the new component!</h1>;
+}

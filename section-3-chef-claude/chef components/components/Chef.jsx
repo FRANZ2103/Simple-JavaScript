dde6a96c962332e4727ec03219b6a4292af12e15
FRANZ2103 <96021965+FRANZ2103@users.noreply.github.com>
2025-10-06 05:35:57 +0800

    Ternary for Conditional state

--- section-3-chef-claude/chef components/components/Chef.jsx
+++ section-3-chef-claude/chef components/components/Chef.jsx
@@ -1,27 +1,32 @@
+import { useState } from "react";
+import React from "react";
 export default function Chef(){
    const ingredients = ["Chicken", "Oregano", "Tomatoes"] 
    const listedIngredients =ingredients.map((ingredient) =>(
-    <li key={ingredient}> {ingredient} </li>
+        <li key={ingredient}> {ingredient} </li>
     ))
-    function handleSubmit(event){
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget)
-        const newIngredient = formData.get("ingredient")
-        ingredients.push(newIngredient)
-        console.log(ingredients)
-        return(
-            console.log(listedIngredients)
-        )
 
+
+
+    const [isGoingOut, setIsGoingOut] = React.useState(true)
+    function handleClick(){
+        {isGoingOut ? setIsGoingOut(isGoingOut => false) : setIsGoingOut(isGoingOut => true)}
     }
+
+    function handleSubmit(event){    }
     return(
         <main>
             <form className="input-section" onSubmit={handleSubmit}>
                 <input className="input-field" type="text" placeholder="e.g. oregano" name="ingredient"/>
-                <button className="add-ingredient-btn" >Add Ingredient</button>
+                <button className="add-ingredient-btn">Add Ingredient</button>
             </form>
+                <button className="add-ingredient-btn" onClick={handleClick}>{isGoingOut ? "Yes" : "No"}</button>
+
+            <h1></h1>
+            
+
             <ul>
-               {listedIngredients}
+               {/* {newList} */}
             </ul>
         </main>
     )

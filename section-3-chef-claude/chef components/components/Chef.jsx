de1cd6ae37d2c6969fa64072cf9c78f829d3bf06
FRANZ2103 <96021965+FRANZ2103@users.noreply.github.com>
2025-11-04 13:57:30 +0800

    Added Checkbox

--- section-3-chef-claude/chef components/components/Chef.jsx
+++ section-3-chef-claude/chef components/components/Chef.jsx
@@ -11,15 +11,18 @@ export default function Chef(){
         const fieldIngredient = formData.get("ingredient")
         setIngredients(()=>[...ingredients,fieldIngredient])
         const emplo = formData.get("employmentStatus")
-        console.log(emplo)    
-        
+        console.log(emplo)            
+        const diet = formData.getAll("diet")
+        console.log(diet)            
+        const color = formData.get("color")
+        console.log(color)            
 
        }
        
        
     return(
         <main>
-            <form className="input-section" action={addIngredient}>
+            <form className="input-section" action={addIngredient} >
                 <input className="input-field" type="text" placeholder="e.g. oregano" name="ingredient"/>
                 <button className="add-ingredient-btn">Add Ingredient</button>
                 <br/>
@@ -29,6 +32,27 @@ export default function Chef(){
                     <label><input type="radio" name="employmentStatus" value="Unemployed"/>Unemployed</label>
                     <label><input type="radio" name="employmentStatus" value="Part-time"/>Part-time</label>
                 </fieldset>
+                <fieldset>
+                    <legend>Dietary restrictions</legend>
+                    <label><input type="checkbox" name="diet"  value="Kosher"/>Kosher</label>
+                    <label><input type="checkbox" name="diet" value="Gluten-free"/>Gluten-free</label>
+                    <label><input type="checkbox" name="diet" value="Vegan"/>Vegan</label>
+                </fieldset>
+                
+                <label htmlFor="color"> Favorite color
+                <fieldset>
+                    <select name="color" id="color" required defaultValue="">
+                        <option value="" disabled>-- Choose Color--</option>
+                        <option value="red">Red</option>
+                        <option value="orange">Orange</option>
+                        <option value="yellow">Yellow</option>
+                        <option value="green">Green</option>
+                        <option value="blue">Blue</option>
+                        <option value="indigo">Indigo</option>
+                        <option value="violet">Violet</option>
+                    </select>
+                </fieldset> 
+            </label>
            
             </form>
            

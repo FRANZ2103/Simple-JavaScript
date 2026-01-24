711358d6dd8a843e9b362d257b6f4a62f44ad495
FRANZ2103 <96021965+FRANZ2103@users.noreply.github.com>
2026-01-24 22:47:29 +0800

    FInished SoundPad challenge for Dynamic CSS Classnames and Passing data

---
+++ section-3-chef-claude/data-passing-components/Pads.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Pads(props){
+    const [on, setOn] = React.useState(props.power)
+    function toggleClick(){
+        setOn (prevOn => !prevOn)
+    }
+    return(
+        <>
+        <button
+        key={props.id}
+        style={{backgroundColor: props.color}}
+        className={on ? "on": null}
+        onClick={toggleClick}
+        ></button>
+        </>
+    )
+}
--- section-3-chef-claude/src/App.jsx
+++ section-3-chef-claude/src/App.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import padsData from "../sound pad components/pads";
 import '../data-passing-components/SoundPadsChallenge/pads.css'
-// const [pads,setPads] = React.useState(padsData)
-const padsJSData = padsData.map((pdata)=>{
-  return(
-    <button key={pdata.id}/>
-    // console.log(pdata.id)
-  )
-})
+import Pads from "../data-passing-components/Pads";
 
-export default function App(){
-  const [pads,setPads] = React.useState(padsData)
+
+export default function App(props){
+  const padRender = padsData.map((toPass=>{
+    return(
+      <Pads
+      key={toPass.id}
+      color={toPass.color}
+      power={toPass.on}
+      
+      />
+    )
+  }))
   return (
 
 
     <div className="pads-container">
-    {padsJSData}
+      {padRender }
     </div>
   )
 }

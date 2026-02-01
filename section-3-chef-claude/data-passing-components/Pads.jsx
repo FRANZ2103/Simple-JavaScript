0e1a0c3bca8866ed9459835364902922428cff96
FRANZ2103 <96021965+FRANZ2103@users.noreply.github.com>
2026-02-02 06:02:48 +0800

    understaning stuff

--- section-3-chef-claude/data-passing-components/Pads.jsx
+++ section-3-chef-claude/data-passing-components/Pads.jsx
@@ -1,32 +1,22 @@
 import React from "react";
 
-// need to know on, and color
-//next is switching on off
-// need to initalize a state setter
-//need to add toggle action 
-
-
 export default function Pads(props){
+    //  console.log(props.whichPad)
     const [on, setOn] = React.useState(props.power)
     function toggleAction(){
         setOn(prevOn =>
             //get prev value
             prevOn = !prevOn
         )
-        console.log("props.whichPad")
     }
     return(
         <>
         <button 
-        style={
-            {
-                backgroundColor:props.color
-            }
-        }
+        style={{backgroundColor:props.color}}
         className={
             on ? "on": null
         }
-        onClick={toggleAction}
+        onClick={()=>props.onPress(props.whichPad)}
         > </button>
         </>
     )
--- section-3-chef-claude/src/App.jsx
+++ section-3-chef-claude/src/App.jsx
@@ -3,15 +3,32 @@ import padsData from "../sound pad components/pads";
 import '../data-passing-components/SoundPadsChallenge/pads.css'
 import Pads from "../data-passing-components/Pads";
 
-// need ng toggle function to be passed
 export default function App(props){
   const [pads,setPads] = React.useState(padsData)
+//     Challenge:
+// Call setPads to update the state of the one pad that was clicked. 
+// Map over the previous pads array, and if the current item you're iterating over has the same id as the 'id' passed
+// to this function, then return a new object with the 'ons value
+// set to the opposite of what it was before.
+// Otherwise (if the ids don't match),
+// just return the previous
+// item as it was, unchanged.'
+  
+  function appToggle(whichPad){
+//think in data, not components
 
-  function appToggle(){
-    console.log("clicked!")
+//I must update the on data
+setPads( prevPad => prevPad.map(pad=>{
+  if (pad.id === whichPad){
+    return{
+    ...pad,
+    on: !pad.on}
+  }return pad
+})
+)
+   
   }
 
-//  need to map on JS file to render Pads.jsx multiple times
 const padRender = padsData.map((pDataFromJS)=>{
   return(
     <Pads
@@ -20,6 +37,7 @@ const padRender = padsData.map((pDataFromJS)=>{
     color={pDataFromJS.color}
     whichPad={pDataFromJS.id}
     onPress={appToggle}
+    allData = {pDataFromJS}
     
     />
   )

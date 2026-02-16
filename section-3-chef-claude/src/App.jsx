cc059030d95c1d7965023028559daff686ae0310
FRANZ2103 <96021965+FRANZ2103@users.noreply.github.com>
2026-02-17 02:54:00 +0800

    Without comments

--- section-3-chef-claude/src/App.jsx
+++ section-3-chef-claude/src/App.jsx
@@ -4,27 +4,16 @@ import '../data-passing-components/SoundPadsChallenge/pads.css'
 import Pads from "../data-passing-components/Pads";
 
 export default function App(props){
-//For later, we need the toggle pad to:
-// detect the whichpad id, once matched, switch the value
-
-
   const [pads,setPads] = React.useState(padsData)// You already used the JS File for initlazation here
- // After that, render using the pads above
 
 function togglePad(whichPad){
-  console.log("Hello World")
-  console.log(whichPad)
-  // map first?
-  //state settir, then sa loob get prev value, return => prevpad na mapped nung pads. 
-  //Afterwards, don sa loob ung if statement
   setPads (prevPads =>prevPads.map(pad=>{
-    
-    // console.log(pad.id)
     if (whichPad === pad.id){
-      console.log(pad.id + "" + whichPad) //Match found confirmer
+      console.log(pad.id + "" + whichPad)
       return {...pad, on: !pad.on      }
-    }else
-{return pad
+    }
+    else{
+      return pad
   
 }
 

7a462c46911514a7018b23f632b7a2334a447a2b
FRANZ2103 <96021965+FRANZ2103@users.noreply.github.com>
2026-03-01 18:07:58 +0800

    Back to CHef Claude

--- section-3-chef-claude/src/App.jsx
+++ section-3-chef-claude/src/App.jsx
@@ -1,54 +1,15 @@
 import React from "react";
-import padsData from "../sound pad components/pads";
-import '../data-passing-components/SoundPadsChallenge/pads.css'
-import Pads from "../data-passing-components/Pads";
+import Chef from '../chef components/components/Chef.jsx'
+import ChefHeader from '../chef components/components/ChefHeader.jsx'
 
-export default function App(props){
-  const [pads,setPads] = React.useState(padsData)// You already used the JS File for initlazation here
+export default function App(){
 
-function togglePad(whichPad){
-  setPads (prevPads =>prevPads.map(pad=>
-    // if (whichPad === pad.id){
-    //   console.log(pad.id + "" + whichPad)
-    //   return {...pad, on: !pad.on      }
-    // }
-    // else{
-    //   return pad
-    whichPad === pad.id ? {...pad, on: !pad.on} : pad
-
-
-  /**
-   * Update the rendering of pad sort of (setPads) from its current value (prevPads)
-   * To do this, we take prevPads as input => output the new array by mapping prevPads and
-   * for each pad object we use the if else logic attached. If it is the target id (whichPad),
-   * copy all of it's current properties and alter the on property. Else, if it is not the target pad, 
-   * return the same.
-   * 
-   * Doing this, will completely replace the target pad. It will use most of the current properties save for one
-   * 
-   * If you are using ternary, either do not envelope the statement using { } or else add return at the beginning
-   */
-
-
-))
-  
-}
- const padRender = pads.map(pads=>
-  //pass the props
-  <Pads
-  key = {pads.id}
-  power = {pads.on}
-  color = {pads.color}
-  whichPad ={pads.id}
-  onPress = {togglePad}
-
-  
-  />
-
- )
+ 
+ 
   return (
-    <div className="pads-container">
-    {padRender}
-    </div>
+    <>
+    <ChefHeader/>
+    <Chef/>
+    </>
   )
 }
--- section-3-chef-claude/src/main.jsx
+++ section-3-chef-claude/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-// import '../chef components/components/index.css'
+import '../chef components/components/index.css'
 // import '../contact-components/Contact.css'
 // import '../sound pad components/soundpad.css'
 // import '../form-data-components/form.css'
